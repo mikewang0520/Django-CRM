0e37e70ff1fcaa3295a0ee70b0bd5d5c2602d770
mikewang0520 <mikewang2000@outlook.com>
2024-01-08 12:16:24 -0500

    set up login layout on homepage

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout # http://localhost:8000/admin/
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
+
     return render(request, 'home.html', {})
+
+
+
+def logout_user(request):
+    pass
